91799aff7b9ae8704613948bf8067005046ca913
DATA8320-07 <DATA8320-07@DESKTOP-RBM4S9B>
2020-05-08 11:22:02 +0900

    깃허브 체험 2

--- javaprj/src/javaprj/javaprj.java
+++ javaprj/src/javaprj/javaprj.java
@@ -5,7 +5,9 @@ public class javaprj {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hello 심대보");
+		System.out.println("hello 심대보2");
+		
+		System.out.println("깃허브 실험");
 	}
 
 }
