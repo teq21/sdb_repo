1f2275551d03f5d0eb37f95e7271e104e14915ab
sdb00 <sdb00@121.167.150.146>
2020-05-11 22:50:30 +0900

    uni_ex6

---
+++ uni_Ex6/src/uni_Ex6/uni_ex6.java
@@ -0,0 +1,20 @@
+package uni_Ex6;
+
+public class uni_ex6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a;
+float b;
+
+a=(int)123.45f;  //a의 정수 변수 저장소 실수를 넣으려하니 (int)를 붙여 오류방지
+b=200;      // 실수 저장소에 정수를 넣어서 오류가ㅏ난다  b=(float)200; 으로 방지
+
+System.out.printf("a의값==>%d \n",a);
+System.out.printf("b의값==>%f \n",b); 
+
+
+
+	}
+
+}
