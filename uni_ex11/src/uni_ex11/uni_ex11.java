0b752dabe947844b535d1e53e8f1d45c38253e7c
sdb00 <sdb00@121.167.150.146>
2020-05-12 00:00:04 +0900

    자바는 문자열함수 String을 쓴다 uni_ex11

---
+++ uni_ex11/src/uni_ex11/uni_ex11.java
@@ -0,0 +1,18 @@
+package uni_ex11;
+
+public class uni_ex11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+String str1="IT COOKBOOK입니다.";
+String str2="10";
+String str3="20";
+
+str1="java 입니다.";	
+
+System.out.printf("%s \n",str1);
+System.out.printf("%s \n" ,str2+str3);
+	}
+
+}
