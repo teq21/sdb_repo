538c9176daa12c8a1e9c443dfa51aeb5a46f7f1f
sdb00 <sdb00@121.167.150.146>
2020-05-11 22:09:02 +0900

    uni_ex3

---
+++ uni_ex3/src/uni_ex3/uni_ex3.java
@@ -0,0 +1,9 @@
+
+public class uni_ex3 {
+
+	public static void main(String[] args) {
+
+	System.out.printf("%d %d " ,100+200 );
+	
+           }
+}
---
+++ uni_ex3/src/uni_ex3/university_ex3.java
@@ -0,0 +1,12 @@
+package uni_ex3;
+
+public class university_ex3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.printf("%d+%d=%d",100,200,300);
+System.out.printf("\n");
+System.out.printf("%d/%d=%f",100,200,0.5);
+	}
+
+}
