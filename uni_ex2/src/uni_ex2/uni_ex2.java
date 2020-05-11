13d4a536a1c5e24b3bfa2767b8ad8f1b4c44c432
sdb00 <sdb00@121.167.150.146>
2020-05-11 21:49:42 +0900

    uni_ex2

---
+++ uni_ex2/src/uni_ex2/uni_ex2.java
@@ -0,0 +1,13 @@
+package uni_ex2;
+
+public class uni_ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("%d",100,200);
+		System.out.printf("\n");
+		System.out.printf("%d  %d",100);
+		System.out.printf("\n"); //%d의 개수와 서식의개수.문자 의 개수가 같아야함
+	}
+
+}
