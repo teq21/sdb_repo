450a6837e4e422c7d782f78a8078ca137735fb85
sdb00 <sdb00@121.167.150.146>
2020-05-11 22:25:45 +0900

    uni_ex4

---
+++ uni_ex4/src/uni_ex4/uni_ex4.java
@@ -0,0 +1,12 @@
+package uni_ex4;
+
+public class uni_ex4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.printf("%d/%d=%f \n" ,100,200,0.5);
+System.out.printf("%c %c \n", 'a','K');
+System.out.printf("%s %s \n","IT CookBook" ,"JAVA");
+	}
+
+}
