81131e82ea929fb3f90357d66317044666a3f519
sdb00 <sdb00@121.167.150.146>
2020-05-11 23:07:59 +0900

    변수선언 uni_ex7

---
+++ uni_ex7/src/uni_ex7/uni_ex7.java
@@ -0,0 +1,20 @@
+package uni_ex7;
+
+public class uni_ex7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a,b;
+float c,d;
+
+a=100;
+b=a;
+
+c=111.1f;
+d=c;
+
+System.out.printf("a,b의값==>>%d %d \n",a,b);
+System.out.printf("c,d의 값==>%5.1f, %5.1f\n" ,c,d);
+	}
+
+}
