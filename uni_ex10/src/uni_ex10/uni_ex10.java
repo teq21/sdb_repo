6241560415d60fab362012f85c175b39ea9946ca
sdb00 <sdb00@121.167.150.146>
2020-05-11 23:48:17 +0900

    boolean 참과거짓을 나타내는함수 uni_ex10

---
+++ uni_ex10/src/uni_ex10/uni_ex10.java
@@ -0,0 +1,16 @@
+package uni_ex10;
+
+public class uni_ex10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+boolean boo1,boo2;
+
+boo1=true;
+System.out.printf("%s \n",boo1);
+
+boo2=(10==20);
+System.out.printf("%s\n",boo2);
+	}
+
+}
