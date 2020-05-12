c6a48b6959dde4a85b46d510057f0d42555972c1
sdb00 <sdb00@121.167.150.146>
2020-05-12 22:42:02 +0900

    논리연산자의 이헤 uni_ex17

---
+++ uni_ex17/src/uni_ex17/uni_ex17.java
@@ -0,0 +1,18 @@
+package uni_ex17;
+
+public class uni_ex17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num1=100, num2=-200;
+
+boolean a=(num1 !=0);   //관계연산자의 결과느 t/f 로나오고 boolean 함수의 값도 t/f 로 나옴
+
+boolean b=(num2 !=0);
+
+System.out.printf("상수의 and 연산:%s \n",a&&b);
+System.out.printf("상수의 or 연산:%s \n",a||b);
+System.out.printf("상수의 and 연산:%s \n",!a);
+	}
+
+}
