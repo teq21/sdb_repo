df240b572832a4cf464e5a46e4f29a0a395d868d
sdb00 <sdb00@121.167.150.146>
2020-05-12 22:07:15 +0900

    전치 연산 .후치 연산 uni_ex14

---
+++ ex_uni14/src/ex_uni14/uni_ex14.java
@@ -0,0 +1,18 @@
+package ex_uni14;
+
+public class uni_ex14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10,b;
+
+b=a++;
+System.out.printf("%d \n",b);
+System.out.printf("%d \n",a);
+	
+	b=++a;
+	System.out.printf("%d \n",b);
+	System.out.printf("%d \n",a);
+	}
+
+}
