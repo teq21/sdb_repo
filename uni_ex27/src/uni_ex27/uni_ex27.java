21aa0d47394be13c11a50b3bacd407a93d92983b
sdb00 <sdb00@121.167.150.146>
2020-05-14 23:50:18 +0900

    uni_ex27

---
+++ uni_ex27/src/uni_ex27/uni_ex27.java
@@ -0,0 +1,13 @@
+package uni_ex27;
+
+public class uni_ex27 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i;
+
+for(i=1;i<=5;i++)
+{System.out.printf("%d \n",i);}
+	}
+
+}
