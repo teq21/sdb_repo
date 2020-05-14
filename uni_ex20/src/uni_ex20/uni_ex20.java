5b55b3b25754315cb6db7713f7d36383ea884abb
sdb00 <sdb00@LAPTOP-GJNOKTR3>
2020-05-14 21:51:08 +0900

    if else 문 uni_EX20

---
+++ uni_ex20/src/uni_ex20/uni_ex20.java
@@ -0,0 +1,16 @@
+package uni_ex20;
+
+public class uni_ex20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		int a=200;
+		if(a<100)
+			
+			System.out.printf("100보다 작군요..\n");
+		else 
+			System.out.printf("100보다 크군요..\n");
+	}
+
+}
