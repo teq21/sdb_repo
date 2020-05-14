7b53a4a4ef087285a757531a9fe6c8991fcc9329
sdb00 <sdb00@121.167.150.146>
2020-05-15 00:17:27 +0900

    홀수의 합  uni_ex29

---
+++ uni_ex29/src/uni_ex29/uni_ex29.java
@@ -0,0 +1,16 @@
+package uni_ex29;
+
+public class uni_ex29 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int hap=0;
+int i;
+
+for(i=501;i<=999;i=i+2)
+{ hap+=i;}
+
+System.out.printf("500에서 1000까지의 홀수의합:%d \n",hap);
+	}
+
+}
