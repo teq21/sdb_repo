19561e5573ee8bfcc7c8503107e8033310c64ad3
sdb00 <sdb00@LAPTOP-GJNOKTR3>
2020-05-14 21:34:11 +0900

    uni_ex18 if 조건문 예시

---
+++ uni_ex18/src/uni_ex18/uni_ex18.java
@@ -0,0 +1,13 @@
+package uni_ex18;
+
+public class uni_ex18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=99;
+       
+       if(a<100)
+    	   System.out.printf("100보다 작군요..\n");   
+	}
+
+}
