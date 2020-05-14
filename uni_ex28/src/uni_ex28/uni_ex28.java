cc6cd75950680722af124d1cacd1499b015c2e1b
sdb00 <sdb00@121.167.150.146>
2020-05-15 00:09:39 +0900

    1부터 10까지의합 uni_ex28

---
+++ uni_ex28/src/uni_ex28/uni_ex28.java
@@ -0,0 +1,14 @@
+package uni_ex28;
+
+public class uni_ex28 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int hap=0;
+int i;
+for(i=1;i<=10;i++)
+{ hap+=i;}
+	
+	System.out.printf("1부터10까지의합:%d \n",hap);
+	}
+}
