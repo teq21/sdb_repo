d1cbb0a2d8c28ab1a784b32f64bb55bb2213ae10
sdb00 <sdb00@LAPTOP-GJNOKTR3>
2020-05-14 23:34:53 +0900

    for문 기초 uni_ex25

---
+++ uni_ex25/src/uni_ex25/uni_ex25.java
@@ -0,0 +1,13 @@
+package uni_ex25;
+
+public class uni_ex25 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i;
+
+for(i=0;i<5;i++) 
+{   System.out.printf("심대보입니다  \n");}
+	}
+
+}
