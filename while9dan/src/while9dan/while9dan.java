709c573bd0a6c77980a4d319eeb333ea71a59a67
sdb00 <sdb00@220.76.87.121>
2020-06-02 23:39:33 +0900

    d

---
+++ while9dan/src/while9dan/while9dan.java
@@ -0,0 +1,25 @@
+package while9dan;
+
+public class while9dan {
+
+	public static void main(String[] args) {
+		int dan=2;
+		int i=1;
+		while(dan<=9) {
+			if(dan==3||dan==8)
+			{System.out.println("<"+dan+"´Ü>");}
+			 
+			i=1;
+			while(i<=9)
+			{System.out.println(dan+"x"+i+"="+dan*i);
+			
+			i++;}
+			dan++;
+		}
+	  
+
+	}
+
+}
+  
+ 
