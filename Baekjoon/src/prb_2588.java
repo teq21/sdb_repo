f792060c663f8a123fa308bd8a7932ac1785f8c5
sdb00 <sdb00@220.76.87.121>
2020-06-02 23:27:52 +0900

    3자리수 곱셈 출력 백준 2588

---
+++ Baekjoon/src/prb_2588.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class prb_2588 {
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		
+		int a,b;
+		a=s.nextInt();
+		b=s.nextInt();
+
+		System.out.println(a*(b%10));
+		System.out.println(a*(b%100/10));
+		System.out.println(a*(b/100));
+		System.out.println(a*b);
+		
+	}
+
+}
