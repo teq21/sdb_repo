bbcc1739d984535d6c5bff72ace9b34b177cec97
sdb00 <sdb00@LAPTOP-GJNOKTR3>
2020-06-03 12:28:13 +0900

    백준 9498번 if문 성적매기기

---
+++ Baekjoon/src/prb_9498.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class prb_9498 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+         int a;
+		a=s.nextInt();
+          if(a>89) 
+        	  System.out.println("A");
+          else if(a>79) 
+        	  System.out.println("B");
+          else if(a>69) 
+        	  System.out.println("C");
+          else if(a>59) 
+        	  System.out.println("D");
+          else 
+        	  System.out.println("F");
+	
+	}
+
+}
