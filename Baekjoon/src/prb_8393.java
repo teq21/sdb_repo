d934169d0d93c607d30e7472a8c485c6304d77c1
sdb00 <sdb00@hanmail.net>
2020-06-04 11:04:21 +0900

    백준 8393 for문출력

---
+++ Baekjoon/src/prb_8393.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class prb_8393 {
+
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	 int n;
+	 n=s.nextInt();
+	 int i;
+	 int hap=0;
+	 for(i=1;i<=n;i++)
+	 {
+		   hap=hap+i;
+	
+	}
+	   System.out.println(hap);
+
+}
+}
