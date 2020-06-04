5884598164622764a0b35918c4bab7806f8da79a
sdb00 <sdb00@hanmail.net>
2020-06-04 11:03:15 +0900

    백준 2741 for문출력

---
+++ Baekjoon/src/prb_2741.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class prb_2741 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int N;
+		N=s.nextInt();
+		
+		int i;
+		for(i=1;i<=N;i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
