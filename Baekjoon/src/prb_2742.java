807bede3388b48757fcd68549e59e20ea56cb79b
sdb00 <sdb00@hanmail.net>
2020-06-04 11:10:47 +0900

    백준 2742 for문출력

---
+++ Baekjoon/src/prb_2742.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class prb_2742 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int N;
+		N=s.nextInt();
+		
+		int i;
+		for(i=N;i>0;i--)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
