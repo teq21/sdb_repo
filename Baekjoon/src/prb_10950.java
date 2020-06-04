73220c2c99946fd5f8c58ad5fb87c022f69f2924
sdb00 <sdb00@hanmail.net>
2020-06-04 10:50:20 +0900

    백준10950 for문

---
+++ Baekjoon/src/prb_10950.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class prb_10950 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a,b,i,j;
+		i=s.nextInt();
+		
+		for(j=1;j<i;j++)
+		{     
+			a=s.nextInt();
+			b=s.nextInt();
+			System.out.println(a+b);
+		}
+		
+	    
+	}
+
+}
