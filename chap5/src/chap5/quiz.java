e6a4377d87ec136036292ba060be62a2575cb737
sdb00 <sdb00@hanmail.net>
2020-06-04 10:23:21 +0900

    ff

--- chap5/src/chap5/quiz.java
+++ chap5/src/chap5/quiz.java
@@ -5,18 +5,20 @@ public class quiz {
 	public static void main(String[] args) {
 		//1부터 100까지의 합
 
-		int aa[]=new int[100];
+		int aa[]=new int[90];
 		
 		
-		for(int i=0;i<100;i++) {
-			aa[i]=i+1;
+		for(int i=0;i<90;i++) {
+			aa[i]=i+11;
 			
 		}
 		int sum=0;
-		int hap=0;
-		while(hap<100) {
-			sum=sum+aa[hap];
-		hap++;}
+		int i=0;
+		while(i<90) {
+			System.out.println("num["+i+"]:"+aa[i]);
+			sum=sum+aa[i];
+		i++;}
+		
 		System.out.println(sum);
 	}
     
