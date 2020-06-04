88fd558e56d94782450a362ce3831cf4ad72e689
sdb00 <sdb00@LAPTOP-GJNOKTR3>
2020-06-04 09:02:00 +0900

    백준 2739 for문 구구단 출력

---
+++ Baekjoon/src/prb_2739.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class prb_2739 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int N;
+		N = s.nextInt();
+		
+		for(int i=1;i<10;i++) {
+			System.out.println(N+"*"+i+"="+N*i);
+			
+		}
+	}
+
+}
