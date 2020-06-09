3fc25534b1ef41795595f38dbafb5fc059f0ff13
sdb00 <sdb00@hanmail.net>
2020-06-09 13:50:10 +0900

    벡즌 10871 for문출력

---
+++ Baekjoon/src/prb_10871.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class prb_10871 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner s=new Scanner(System.in);
+     int a,b,c;
+     a=s.nextInt();
+     b=s.nextInt();
+    
+    
+    for(c=1;c<=a;c++)
+    {
+    c=s.nextInt();
+    if(c<b)
+    {
+    	System.out.print(c+" ");
+    }
+    }
+    
+     
+    
+     }
+     
+	
+
+}
